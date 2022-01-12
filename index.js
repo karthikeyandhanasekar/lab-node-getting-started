847236f57a3e5153e9fa0874a468c2ec8c98591a
Karthik <dkkarthik2000@gmail.com>
2022-01-12 10:54:35 +0530

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 class SortedList {
   constructor() {
-  
+    this.items = []
+    this.length = 0
+
   }
-  add() {
-   
+  add(value) {
+    this.items.push(value)
+    this.items.sort((n1, n2) => n1 - n2);
+    this.length = this.items.length
   }
-  get() {
+  get(index) {
+    if (index > length) throw new Error("OutOfBounds")
+    else return this.items.indexOf(index)
 
   }
   max() {
+    if(this.length == 0) throw new Error("EmptySortedList");
+    else return Math.max(...this.items)
 
   }
   min() {
+    if(this.length == 0) throw new Error("EmptySortedList");
+    else  return Math.min(...this.items)
+
 
   }
   avg() {
-    
-   
+    if(this.length == 0) throw new Error("EmptySortedList");
+    else return (this.items.reduce((output,ele)=> output+ele) / this.length)
+
+
+
   }
 
   sum() {
-  
+    if(this.length == 0) throw new Error("EmptySortedList");
+    else  return this.items.reduce((output,ele)=> output+ele)
+
+
   }
-  
+
 };
 
 module.exports = SortedList;
